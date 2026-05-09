ac280bea6b5d796bb6bb4705e28d261565d76976
M31i55a <melysgioda34@gmail.com>
2026-05-09 06:15:31 +0000

    args error handeled with exit code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config: Config = Config::new(&args);
+    // print the error message and exit the program if the arguments are not valid
+    let config: Config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents =
-        fs::read_to_string(&config.filename).expect("Something went wrong reading the file");
+    let contents = fs::read_to_string(&config.filename).expect("Something went wrong reading the file");
 
     println!("With text: \n{}", contents);
 }
@@ -20,16 +24,16 @@ struct Config {
     filename: String,
 }
 
-impl Config {
+impl Config{
     // the new function is a convention for a constructor in Rust
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
         if args.len() < 3 {
-            panic!("Not enough arguments");
+            return Err("Not enough arguments");
         }
-
+        
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
